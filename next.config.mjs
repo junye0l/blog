7780b30fa2a48753ae3a820f90051f66e73bdefb
junye0l <junyeolkim00@gmail.com>
2026-01-04 18:53:54 +0900

    fix: use webpack in next 16

--- next.config.mjs
+++ next.config.mjs
@@ -70,9 +70,6 @@ export default () => {
     reactStrictMode: true,
     trailingSlash: false,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    eslint: {
-      dirs: ['app', 'components', 'layouts', 'scripts'],
-    },
     images: {
       remotePatterns: [
         {
