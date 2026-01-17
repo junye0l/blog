1e9ba3b46c1388052dbd2a53924e233e5fe03d40
junye0l <junyeolkim00@gmail.com>
2026-01-17 20:06:10 +0900

    docs : yamoyo url fix

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -27,7 +27,7 @@ const projectsData: Project[] = [
     title: 'yamoyo',
     description: '팀 프로젝트 시작에 필요한 모든 결정을 재밌고 빠르게 끝내는 팀 초기 세팅 서비스',
     imgSrc: '/static/images/coming-soon.png',
-    href: 'https://github.com/junye0l',
+    href: 'https://github.com/yamoyo/yamoyo_FE',
     type: '팀',
   },
 ]
